d30f3c07a8e45c3cd317d42164315e1c681853a8
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-16 00:58:50 -0600

    routes

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const cors = require('cors');
-
+const { db } = require('./database/db');
+const {readdirSync} = require('fs')
 const app = express()
+
 require('dotenv').config()
 
 const PORT = process.env.PORT
@@ -10,8 +12,14 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cors())
 
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1',require('./routes/'+ route)))
+
 const server = () => {
-   console.log('you are listening to port:', PORT);
+  db()
+   app.listen(PORT, ()=> {
+       console.log('Listening to port:', PORT)
+   })
 
 }
 
