275f15b78abbea42f63c56fb49dc2bd4cfe2d9fd
soumyayalahankaajjegowdagit config --global user.name soumyayalahankaajjegowda <soumya.ajjegowda@gmail.com>
2025-03-16 16:58:39 -0600

    using hooks

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { GlobalStyle } from './styles/GlobalStyle.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <GlobalStyle />
     <App />
-  </StrictMode>,
+  </React.StrictMode>
 )
